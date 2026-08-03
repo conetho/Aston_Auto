e4d5cf6b0fad67e41efd9c668743fed7edb66ae5
conetho <104602427+conetho@users.noreply.github.com>
2026-08-03 14:34:50 +0700

    Finished HW

---
+++ src/main/java/org/example/Park.java
@@ -0,0 +1,60 @@
+package org.example;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Park {
+    private String parkName;
+    private List<Attraction> attractions = new ArrayList<>();
+
+    public Park(String parkName) {
+        this.parkName = parkName;
+    }
+
+    public class Attraction {
+        private String name;
+        private String workingHours;
+        private int price;
+
+        public Attraction(String name, String workingHours, int price){
+            this.name = name;
+            this.workingHours = workingHours;
+            this.price = price;
+        }
+
+        public void displayAttractionInfo(){
+
+            System.out.println("Аттракцион: " + name);
+            System.out.println("Время работы: " + workingHours);
+            System.out.println("Цена: " + price + " руб.");
+            System.out.println("-------------------");
+        }
+    }
+
+    public void addAttraction(String name, String workingHours, int price) {
+        Attraction attraction = new Attraction(name, workingHours, price);
+        attractions.add(attraction);
+    }
+    public void displayParkInfo(){
+        System.out.println("Парк: " + parkName);
+        System.out.println("Список аттракционов:");
+        System.out.println("-------------------");
+        for (Attraction a : attractions) {
+            a.displayAttractionInfo();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Park centralPark = new Park("Центральный парк");
+        Park newPark = new Park("Новый парк");
+        centralPark.addAttraction("Колесо обозрения", "10:00 - 22:00", 300);
+        centralPark.addAttraction("Американские горки", "11:00 - 21:00", 450);
+        newPark.addAttraction("Карусель", "09:00 - 22:00", 150);
+        newPark.addAttraction("Американские горки", "11:00 - 21:00", 450);
+
+        centralPark.displayParkInfo();
+        newPark.displayParkInfo();
+    }
+}
+
+
