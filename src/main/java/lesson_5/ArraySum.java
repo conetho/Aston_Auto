c944d885f571de2c2abaff7adcc0aab057bcdf81
conetho <104602427+conetho@users.noreply.github.com>
2026-08-04 16:46:56 +0700

    All done

---
+++ src/main/java/lesson_5/ArraySum.java
@@ -0,0 +1,26 @@
+package lesson_5;
+
+public class ArraySum {
+
+    public static int sumArray(String[][] arr) throws MyArraySizeException, MyArrayDataException {
+        int sum = 0;
+        if (arr.length != 4) {
+            throw new MyArraySizeException("Неверный размер массива: количество строк должно быть 4.");
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].length != 4) {
+                throw new MyArraySizeException("Неверный размер массива: в строке " + i + " количество столбцов " +
+                        "должно быть 4.");
+            }
+            for (int j = 0; j < arr[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(arr[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Неверные данные в ячейке [" + i + "][" + j + "]: " + arr[i][j] );
+                }
+            }
+        }
+        return sum;
+    }
+}
