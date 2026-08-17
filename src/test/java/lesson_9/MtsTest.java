d5b0af0760503bef88af03048805882fe2d29d7d
Alex P <aleksandrnosov6044@gmail.com>
2026-08-17 20:47:59 +0700

    Progress achieved

--- src/test/java/lesson_9/MtsTest.java
+++ src/test/java/lesson_9/MtsTest.java
@@ -83,9 +83,7 @@ public class MtsTest {
         assertTrue(submitButtonElement.isEnabled(), "Кнопка 'Продолжить' не активна");
         submitButtonElement.click();
 
-        // Проверка перехода к оплате (появление фрейма Белгазпромбанка)
         WebElement appPaymentContainer = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='payment-widget-app']")));
         assertTrue(appPaymentContainer.isDisplayed(), "Фрейм оплаты не появился после нажатия кнопки 'Продолжить'");
     }
-
 }
