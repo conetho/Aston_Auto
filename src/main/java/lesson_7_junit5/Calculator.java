21b57b93d3e4829f081425b9d04f83cad4a7c9d1
Alex P <aleksandrnosov6044@gmail.com>
2026-08-20 22:50:11 +0700

    Progress achieved

--- src/main/java/lesson_7_junit5/Calculator.java
+++ src/main/java/lesson_7_junit5/Calculator.java
@@ -5,12 +5,15 @@ public class Calculator {
     public int add(int a, int b) {
         return a + b ;
     }
+
     public int subtract(int a, int b) {
         return a - b ;
     }
+
     public int multiply(int a, int b) {
         return a * b;
     }
+
     public double divide(int a, int b) {
         if (b == 0) {
             throw new ArithmeticException("Вселенная уничтожена (нельзя делить на ноль)");
